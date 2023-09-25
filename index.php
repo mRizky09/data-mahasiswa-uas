ba4c7fdb3c192f55bc0ebdbd7c081eff3cb0cfd4
mRizky09 <mrizky942003@gmail.com>
2023-09-25 12:10:52 +0700

    test

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $no = 0;
     <link rel="stylesheet" href="./assets/css/style1.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa</title>
+    <title>Data MahasiswA</title>
 </head>
 
 <body>
